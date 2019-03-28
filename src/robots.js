fc0d55409b2df448ce8be8f824cac17b17c7cfeb
kyle <kyle10@kyles-iMac.local>
2019-03-28 12:51:41 +0800

    starting project

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id: 1,
+	name: 'France Patrick',
+	username: 'France',
+	email: 'francecutie@gmail.com', 
+},
+{
+	id: 2,
+	name: 'Ronilo Ramada',
+	username: 'ronix',
+	email: 'ronixcutie@gmail.com',
+},
+{
+	id: 3,
+	name: 'Eem Cocaloon',
+	username: 'Eem',
+	email: 'eemcutie@gmail.com',
+},
+{
+	id: 4,
+	name: 'Ralph Godfrey',
+	username: 'Boss Chase',
+	email: 'chasecutie@gmail.com', 
+},
+{
+	id: 5,
+	name: 'James Fornis',
+	username: 'James',
+	email: 'jamescutie@gmail.com', 
+},
+]
